7b377999cbd4392b6cf37e17aec87ce3147ce8a1
Colin Merkel <colinwm@github.com>
2023-01-29 17:32:13 -0500

    colinwm-dotfiles

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 sudo chsh -s "$(which zsh)" "$(whoami)"
 cp -rf $(pwd)/.zshrc $HOME/.zshrc
 cp -rf $(pwd)/.tmux.conf $HOME/.tmux.conf
-cp -rf $(pwd)/.configs $HOME
+cp -rf $(pwd)/.config $HOME
 cp -rf $(pwd)/.oh-my-zsh $HOME
 
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
@@ -22,4 +22,6 @@ if [ -d "/workspaces/github/bin/" ]; then
     sudo ln -s /workspaces/github/bin/solargraph /usr/local/bin/solargraph
 fi
 
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 nvim --headless +PlugInstall +qall
