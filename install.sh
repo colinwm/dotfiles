7fd05bdc587a7e9cd51b5e8fed4e07b82b2f7a4f
Colin Merkel <colinwm@github.com>
2023-01-29 17:18:42 -0500

    colinwm-dotfiles

--- install.sh
+++ install.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 
+sudo chsh -s "$(which zsh)" "$(whoami)"
+cp -f $(pwd)/.zshrc $HOME/.zshrc
+cp -f $(pwd)/.tmux.conf $HOME/.tmux.conf
+cp -f $(pwd)/.configs $HOME
+
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install --all
 
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+if [ -d "/workspaces/github/bin/" ]; then
+    sudo ln -s /workspaces/github/bin/npm /usr/local/bin/npm
+    sudo ln -s /workspaces/github/bin/tsc /usr/local/bin/tsc
+    sudo ln -s /workspaces/github/bin/prettier /usr/local/bin/prettier
+    sudo ln -s /workspaces/github/bin/eslint /usr/local/bin/eslint
+    sudo ln -s /workspaces/github/bin/rubocop /usr/local/bin/rubocop
+    sudo ln -s /workspaces/github/bin/srb /usr/local/bin/srb
+    sudo ln -s /workspaces/github/bin/bundle /usr/local/bin/bundle
+    sudo ln -s /workspaces/github/bin/solargraph /usr/local/bin/solargraph
+fi
