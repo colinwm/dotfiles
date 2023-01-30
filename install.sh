205fca25dd3d588a95abea0c296ae221815cedc4
Colin Merkel <colinwm@github.com>
2023-01-29 22:12:20 -0500

    colinwm-dotfiles

--- install.sh
+++ install.sh
@@ -8,16 +8,12 @@ cp -rf $(pwd)/.config $HOME
 cp -rf $(pwd)/gitconfig $HOME/.gitconfig
 cp -rf $(pwd)/.oh-my-zsh $HOME
 cp $(pwd)/tmux-url-select $HOME/bin
-
-mkdir -p ~/.g2
-cp -rf $(pwd)/g2.toml $HOME/.g2/g2.toml
+cp $(pwd)/files $HOME/bin/files
 
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install --all
 
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-
-cargo install g2
 
 if [ -d "/workspaces/github/bin/" ]; then
     sudo ln -s /workspaces/github/bin/npm /usr/local/bin/npm
@@ -29,6 +25,11 @@ if [ -d "/workspaces/github/bin/" ]; then
     sudo ln -s /workspaces/github/bin/bundle /usr/local/bin/bundle
     sudo ln -s /workspaces/github/bin/solargraph /usr/local/bin/solargraph
 fi
+
+# Install g2
+mkdir -p ~/.g2/branches
+cp -rf $(pwd)/g2.toml $HOME/.g2/g2.toml
+cargo install g2
 
 # Detach head of github/github to enable workspaces
 cd /workspaces/github
@@ -45,3 +46,6 @@ sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.
 cd ~/.local/share/nvim/plugged/coc.nvim
 npm install yarn
 ./node_modules/yarn/bin/yarn install
+
+# Install a second time to recognize CoC updates
+/home/linuxbrew/.linuxbrew/bin/nvim --headless +PlugInstall +qall
